3c6afad596ee220b1855fc9cd1646a1300fc924e
RabihMoustapha <mostapharabih59@gmail.com>
2025-01-15 20:28:11 +0200

    Just crazy things like repeat the project :(

--- Frontend/JS/Account/Login.js
+++ Frontend/JS/Account/Login.js
@@ -1,13 +1,14 @@
 const profile = 'http://localhost/IDS/Backend/profile.php';
 const post = 'http://localhost/IDS/Backend/post.php';
-var email = document.getElementById('email');
-var password = document.getElementById('password');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
 
 function login() {
     fetch(profile)
         .then(response => response.json())
         .then(data => isloggedin(data))
-        .catch(error => console.error('Unable to get items.', error));
+        .error(error => console.error('Unable to get items.', error));
 }
 
 function getItem() {
